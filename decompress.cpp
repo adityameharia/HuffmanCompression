5e35e631324719db8f2ba67083a602d42f0a2b6e
DebangshuB <debangshu133@gmail.com>
2021-03-06 17:03:38 +0530

    Added Basic Code for Decoding

--- decompress.cpp
+++ decompress.cpp
@@ -4,7 +4,47 @@ using namespace std;
 
 int main()
 {
-	fstream fin("hamlet_compresses.adi");
-	fstream fout("hamlet_decompresses.txt");
+	ifstream fin("./decompression_sample/sample.adi",ios::in | ios::binary);
+
+	const node *root = ?;
+	node* curr;
+
+
+	char inp;
+	char mask = 128;
+	char move = 0;
+
+	while ( !fin.eof() )
+	{
+		fin.get(inp);
+		cout<<inp<<" ";
+		for ( int i = 0 ; i < 8 ; i++ )
+		{
+			move = inp & mask;
+			move = move >> 8;
+			inp  = inp << 1;
+
+			cout<<((int)move ? 1 : 0)<<" ";
+
+			if ( move)
+				curr = curr->right;
+			else
+				curr = curr->left;
+
+			if ( curr->next )
+			{
+				cout<<curr->data;
+				curr = root;
+			}
+
+
+
+		}
+		cout<<endl;
+		cout<<"-----------------------\n";
+
+	}
+
 }
+
 
