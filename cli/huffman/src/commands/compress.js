b2c128d2feb8bbd7a40bafcc05c87636eb97be69
Aditya Meharia <adityameharia14@gmail.com>
2021-03-10 01:28:37 +0530

    added support for windows

--- cli/huffman/src/commands/compress.js
+++ cli/huffman/src/commands/compress.js
@@ -1,14 +1,28 @@
 const { Command, flags } = require('@oclif/command')
 let execFile = require('child_process').execFile
 const process = require('process');
+const os=require("os")
 
 class CompressCommand extends Command {
   async run() {
     const {args}=this.parse(CompressCommand)
     const {flags} = this.parse(CompressCommand)
-    const name = (flags.name || args["path"])+".wtf"
+    const name = (flags.name || args["path"].split('.')[0])+".wtf"
+
+    let executable;
+
+    if(os.platform()=="linux"){
+      executable="compress.out"
+    }
+    else if(os.platform()=="win32"){
+      executable="compress.exe"
+    }
+    else{
+      console.log("Your os is not supported at the moment.")
+      process.exit(1)
+    }
     
-    execFile(__dirname+"/executables/compress.out", [process.cwd()+"/"+args["path"],name],function (error, stdout, stderr) {
+    execFile(__dirname+"/executables/"+executable, [process.cwd()+"/"+args["path"],name],function (error, stdout, stderr) {
       // This callback is invoked once the child terminates
       // You'd want to check err/stderr as well!
       if(stdout)console.log(stdout)
--- cli/huffman/src/commands/decompress.js
+++ cli/huffman/src/commands/decompress.js
@@ -1,14 +1,28 @@
 const {Command, flags} = require('@oclif/command')
 let execFile = require('child_process').execFile
 const process = require('process');
+const os=require("os")
 
 class DecompressCommand extends Command {
   async run() {
     const {args}=this.parse(DecompressCommand)
     const {flags} = this.parse(DecompressCommand)
-    const name = (flags.name || args["path"])+".txt"
+    const name = (flags.name || args["path"].split('.')[0])+".txt"
+
+    let executable;
+
+    if(os.platform()=="linux"){
+      executable="decompress.out"
+    }
+    else if(os.platform()=="win32"){
+      executable="decompress.exe"
+    }
+    else{
+      console.log("Your os is not supported at the moment.")
+      process.exit(1)
+    }
     
-    execFile(__dirname+"/executables/decompress.out", [process.cwd()+"/"+args["path"],name],function (error, stdout, stderr) {
+    execFile(__dirname+"/executables/"+executable, [process.cwd()+"/"+args["path"],name],function (error, stdout, stderr) {
       // This callback is invoked once the child terminates
       // You'd want to check err/stderr as well!
       if(stdout)console.log(stdout)
